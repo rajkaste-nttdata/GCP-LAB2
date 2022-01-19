0ddeb3c1e7c5b34f8f291e4de1a66b6cfd138dec
Raj Kaste <96291910+rajkaste-nttdata@users.noreply.github.com>
2022-01-20 02:10:01 +0530

    Add files via upload

---
+++ Raj_Kaste_LAB2_Exercise_4.sql
@@ -0,0 +1,20 @@
+with country_from as(
+    SELECT country,ARRAY_AGG(STRUCT(
+        operatingSystem ,browser,rank
+        )LIMIT 3
+    ) as country_rank
+   FROM( SELECT geoNetwork.country as country,device.operatingSystem as operatingSystem,
+    device.browser as browser,
+    ROW_NUMBER()  OVER (
+        PARTITION By geoNetwork.country
+        ORDER BY (count(device.browser)*count(device.operatingSystem)) DESC
+    ) AS rank
+    FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20170801`
+    WHERE device.isMobile = TRUE 
+    group by country,operatingSystem,browser
+)
+WHERE country NOT like '(%)'
+GROUP BY country
+)
+SELECT * FROM country_from
+ORDER BY country
