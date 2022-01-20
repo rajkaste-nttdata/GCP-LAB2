b3ae8b6fdc117554e50c2665d627ad85e223a924
Raj Kaste <96291910+rajkaste-nttdata@users.noreply.github.com>
2022-01-20 16:19:28 +0530

    Update Raj_Kaste_LAB2_Exercise_3.sql

--- Raj_Kaste_LAB2_Exercise_3.sql
+++ Raj_Kaste_LAB2_Exercise_3.sql
@@ -1,3 +1,8 @@
+--Exercise 3
+--Find how many times a page it was visited in July 2017. Group by date the result
+--Public Dataset: bigquery-public-data.google_analytics_sample
+--Table involved: ga_sessions_201707* (wildcard)
+
 SELECT CAST(date as DATE FORMAT 'YYYYMMDD') as date,
 H.page.pagePath as pagePath,
 count(H.page.pagePath) as counter
