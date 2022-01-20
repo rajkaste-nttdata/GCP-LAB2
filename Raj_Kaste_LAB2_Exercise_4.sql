65cb0f6786399f85c6587b33b8ed63261e29029e
Raj Kaste <96291910+rajkaste-nttdata@users.noreply.github.com>
2022-01-20 16:20:46 +0530

    Update Raj_Kaste_LAB2_Exercise_4.sql

--- Raj_Kaste_LAB2_Exercise_4.sql
+++ Raj_Kaste_LAB2_Exercise_4.sql
@@ -1,4 +1,13 @@
-/*
+--Exercise 4
+--1. Find the first 3 most used pairs of OS and Browser from mobile devices for each
+--available country on 1 st August 2017
+--2. Produce a new table with the query result
+--Public Dataset: bigquery-public-data.google_analytics_sample
+--Table involved: ga_sessions_20170801
+--Tip: ARRAY_AGG functions can be useful
+--(see: https://cloud.google.com/bigquery/docs/reference/standard-sql/aggregate_functions)
+
+/*Point 2:
 For Creating a new table for Query result, add 
 CREATE or REPLACE TABLE `dataset.tableID` at the beginning of the query
 
