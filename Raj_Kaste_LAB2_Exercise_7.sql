2757fbaacf0835ba0dd820929b2e2ba30fef15bf
Raj Kaste <96291910+rajkaste-nttdata@users.noreply.github.com>
2022-01-20 16:24:38 +0530

    Update Raj_Kaste_LAB2_Exercise_7.sql

--- Raj_Kaste_LAB2_Exercise_7.sql
+++ Raj_Kaste_LAB2_Exercise_7.sql
@@ -1,3 +1,8 @@
+--Exercise 7
+--Find the most popular programming language
+--Public Dataset: bigquery-public-data.github_repos
+--Table involved: languages
+
 SELECT l.name as language_name, Count(l.name) as count FROM `bigquery-public-data.github_repos.languages`,
 UNNEST(language) as l
 Group by language_name
