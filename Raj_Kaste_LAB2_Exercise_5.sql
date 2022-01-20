2c062ce5a5af6698eccfec9228e6c9e39cb18432
Raj Kaste <96291910+rajkaste-nttdata@users.noreply.github.com>
2022-01-20 15:57:36 +0530

    Update Raj_Kaste_LAB2_Exercise_5.sql

--- Raj_Kaste_LAB2_Exercise_5.sql
+++ Raj_Kaste_LAB2_Exercise_5.sql
@@ -4,6 +4,7 @@ FROM `bigquery-public-data.stackoverflow.users` users
 JOIN
 `bigquery-public-data.stackoverflow.posts_answers` pa
 ON users.id=pa.owner_user_id
+where EXTRACT (YEAR from pa.creation_date)=2010
 group by id_user
 ORDER BY count DESC
 LIMIT 10
