1fe61e036b43fc62bfdd1fd8df73a51bd981449b
Raj Kaste <96291910+rajkaste-nttdata@users.noreply.github.com>
2022-01-20 16:26:28 +0530

    Update Raj_Kaste_LAB2_Exercise_9.sql

--- Raj_Kaste_LAB2_Exercise_9.sql
+++ Raj_Kaste_LAB2_Exercise_9.sql
@@ -1,3 +1,10 @@
+--Exercise 9
+--Find for each commit on Github on a .c file of the Linux kernel, the previous and the next commit
+--Public Dataset: bigquery-public-data.github_repos
+--Table involved: sample_commits
+--Tip: Lag and Lead functions can be useful
+--(see: https://cloud.google.com/bigquery/docs/reference/standard-sql/navigation_functions)
+
 SELECT repo_name, D.new_path as file,
 committer.date as date,
 LAG(commit) OVER(
