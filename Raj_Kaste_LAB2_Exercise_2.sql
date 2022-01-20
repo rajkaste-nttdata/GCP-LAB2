a9b63b7e1455fa9060ca84a75829a887dc1cd3fd
Raj Kaste <96291910+rajkaste-nttdata@users.noreply.github.com>
2022-01-20 16:18:54 +0530

    Update Raj_Kaste_LAB2_Exercise_2.sql

--- Raj_Kaste_LAB2_Exercise_2.sql
+++ Raj_Kaste_LAB2_Exercise_2.sql
@@ -1,4 +1,9 @@
-SELECT 
+--Exercise 2
+--Show a flat result of the pages visited on 1 st August 2017
+--Public Dataset: bigquery-public-data.google_analytics_sample
+--Table involved: ga_sessions_20170801
+
+SELECT
 visitid,visitStartTime,h.page.pageTitle as pageTitle ,h.page.pagePath as pagePath
 FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20170801`,
 UNNEST(hits) AS h;
