8e11b437e9dcad9446cbc5b0ef573945a33896d0
Raj Kaste <96291910+rajkaste-nttdata@users.noreply.github.com>
2022-01-20 16:15:12 +0530

    Update Raj_Kaste_LAB2_Exercise_6.sql

--- Raj_Kaste_LAB2_Exercise_6.sql
+++ Raj_Kaste_LAB2_Exercise_6.sql
@@ -1,10 +1,12 @@
-SELECT P.owner_user_id AS id_user,
-COUNT(S.owner_user_id) as count
-FROM `bigquery-public-data.stackoverflow.stackoverflow_posts` S
-JOIN `bigquery-public-data.stackoverflow.posts_answers` P
-ON S.owner_user_id=P.owner_user_id
-WHERE S.accepted_answer_id IS NOT NULL
-GROUP BY P.owner_user_id,S.creation_date
-HAVING EXTRACT (YEAR FROM S.creation_date)=2010
+SELECT users.id AS id_user,
+COUNT(*) as count
+FROM `bigquery-public-data.stackoverflow.users` users
+INNER JOIN `bigquery-public-data.stackoverflow.posts_answers` P
+ON users.id=P.owner_user_id
+INNER JOIN `bigquery-public-data.stackoverflow.stackoverflow_posts` S
+ON S.accepted_answer_id=P.id
+WHERE S.accepted_answer_id IS NOT NULL and 
+EXTRACT (YEAR FROM S.creation_date)=2010
+GROUP BY id_user
 ORDER BY count DESC 
 LIMIT 10
